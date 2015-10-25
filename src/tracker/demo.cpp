e86f36340a6e4a510a98d76d656973ca54684692
Jonathan Bailey <jonathan@dyn1205-192.wlan.ic.ac.uk>
2015-10-25 20:07:52 +0000

    Apply filter to webcam

--- src/tracker/demo.cpp
+++ src/tracker/demo.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
 #include <stdio.h>
@@ -17,7 +18,7 @@ int main(int argc, char* argv[])
     }
 
     namedWindow("Demo",CV_WINDOW_AUTOSIZE); //create a window
-
+    namedWindow("Norm",CV_WINDOW_AUTOSIZE); //create a window
     while (true)
     {
         Mat frame; //images use the Mat type from openCV
@@ -29,8 +30,25 @@ int main(int argc, char* argv[])
              cout << "Cannot read a frame from video stream" << endl;
              break;
         }
-
-        imshow("Demo", frame); //show the frame
+        Mat kernel_x = (Mat_<double>(3,3) <<
+                        -1, 0, 1,
+                        -2, 0, 2,
+                        -1, 0, 1
+                        );
+        Mat kernel_y = (Mat_<double>(3,3) <<
+                        -1, -2, -1,
+                         0,  0,  0,
+                         1,  2,  1
+                        );
+        
+        Mat G_x;
+        Mat G_y;
+        
+        imshow("Norm", frame);
+        
+        filter2D(frame, G_x, -1, kernel_x);
+        filter2D(frame, G_y, -1, kernel_y);
+        imshow("Demo", G_x); //show the frame
 
         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. Done like this on advice that HighGui requires regular calls to waitKey...
         { 
