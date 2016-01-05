54922551b1d729273f92a3510741637475fcb8fd
sx813 <sx813@ic.ac.uk>
2016-01-05 17:02:17 +0000

    reverse screen direction

--- src/wm/wm.cpp
+++ src/wm/wm.cpp
@@ -32,16 +32,16 @@ void wm::print_attr(Display* d, Window *children_windows, XWindowAttributes *att
 }
 
 void wm::set_focus_screen(Direction direction){
-	Display *d = XOpenDisplay(NULL);
-	if(direction == Left){
-		set_focus_to(d, 1920/2, 1080/2);
-	}
-	else if(direction == Middle){
-		set_focus_to(d, 1920 + 1920/2, 1080/2);
-	}
-	else if(direction == Right){
+    Display *d = XOpenDisplay(NULL);
+    if(direction == Right){
+        set_focus_to(d, 1920/2, 1080/2);
+    }
+    else if(direction == Middle){
+        set_focus_to(d, 1920 + 1920/2, 1080/2);
+    }
+    else if(direction == Left){
         set_focus_to(d, 1920*2 - 1920/2, 1080/2);
-	}
+    }
     XCloseDisplay(d);
 }
 
