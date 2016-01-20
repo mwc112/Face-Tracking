aadf594a572d2ae04622e50e8dd9547df3635a07
Jonathan Bailey <jonathan@dyn1205-192.wlan.ic.ac.uk>
2016-01-20 14:03:21 +0000

    Tidy up wm.h

--- src/wm/wm.h
+++ src/wm/wm.h
@@ -22,30 +22,32 @@ struct point {
 };
 
 class wm {
-
-private:
-	Window ourWindow;
+  
 public:
-
-	/* Enum used for direction being looked in to choose screen */
-	typedef enum Direction {
+	//Enum used for direction being looked in to choose screen
+	enum Direction {
 		Left, Middle, Right, Unknown
-	} Direction;
-
-	Status get_root_windows(Display*, Window**, unsigned int*);Status get_windows_attr(
-			Display*, Window*, int, XWindowAttributes*);
-	void print_attr(Display*, Window*, XWindowAttributes*, int);
-	void set_focus_to(Display*, int, int);
-	std::vector<Window> get_client_window_list(Display*);
+	};
+  
 	void set_focus_screen(Direction);
 
 	wm() {
 		Display *d = XOpenDisplay(NULL);
 		int revert_to_return;
+    //this sleep is required so that the window is open
+    //  when we get our window.
 		usleep(1000000);
 		XGetInputFocus(d, &ourWindow, &revert_to_return);
 		XCloseDisplay(d);
 	}
+  
+private:
+  Status get_root_windows(Display*, Window**, unsigned int*);
+  Status get_windows_attr(Display*, Window*, int, XWindowAttributes*);
+  void print_attr(Display*, Window*, XWindowAttributes*, int);
+  void set_focus_to(Display*, int, int);
+  std::vector<Window> get_client_window_list(Display*);
+  Window ourWindow;
 };
 
 #endif
